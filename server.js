5eed33b0530206e1216a5fde939a49003c86f177
Carolina Ale <redrumjedi@gmail.com>
2021-03-15 22:49:29 -0300

    mongoose initial config

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on("error", (error) => console.erros(error));
+db.once("open", () => console.log("Connected to DB"));
+
 app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 3000);
